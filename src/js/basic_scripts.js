41015e42cdcf91084ea2e86e64107a5d414f7958
radiks <radion91@gmail.com>
2016-07-01 15:15:48 +0300

    asdasdasd

--- src/js/basic_scripts.js
+++ src/js/basic_scripts.js
@@ -146,20 +146,35 @@ function headeButer(menuMobile,toggleMenu){
     }
 }
 
-/* expresion for numbers with spaces */
 
-    function numberWithSpaces(x) {
-        var parts = x.toString().split(".");
-        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-        return parts.join(".");
+function changeViewport(){
+
+    $(window).resize(function(){
+
+        whatViewport();
+
+    });
+
+    function whatViewport(){
+
+        var windowWidth = screen.width;
+        var viewport = $("meta[name=viewport]");
+        viewport.attr('content', 'width=device-width');
+        if(windowWidth<=640){
+            viewport.attr('content', 'width=640');
+        }
+
     }
 
-/* /expresion for numbers with spaces */
+}
+
+
+
 
 /* DOCUMENT READY  */
 $(document).ready(function() {
     //oneHeightItems();
-    $('.footer_placeholder').height($('.footer').outerHeight());
+    //$('.footer_placeholder').height($('.footer').outerHeight());
 
     //goTo();
     //animationBlock($('.setion-animate'));
@@ -167,5 +182,11 @@ $(document).ready(function() {
 
 $(window).resize(function() {
 
-    $('.footer_placeholder').height($('.footer').outerHeight());
+    //$('.footer_placeholder').height($('.footer').outerHeight());
+});
+
+$(window).load(function(){
+
+    changeViewport();
+
 });
--- src/js/develop/develop_4.js
+++ src/js/develop/develop_4.js
@@ -1,8 +1,23 @@
-
+function lend1Timer(){
+     $('#timeto-1').timeTo({
+        //timeTo: new Date(new Date('Fri Jul 08 2016 09:00:00')),
+        timeTo: new Date(new Date(lend1_year,lend1_mounts,lend1_day)),
+        displayDays: 2,
+        lang:'ru',
+        displayCaptions: true,
+        fontFamily:'Lato',
+        fontSize:50
+     });
+    // var newYear = new Date(lend1_year, lend1_mounts-1, lend1_day)
+    // $('#timeto-1').countdown({
+    //     until: newYear,
+    //     regionalOptions:'ru'
+    // });
+}
 
 
 $(document).ready(function(){
-
+    lend1Timer();
 });
 
 $(window).load(function(){
